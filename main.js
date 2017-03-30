511192e7361495a59c436024d8a1fb65f0746cd4
jctrvlr <jcummings5@gmail.com>
2017-03-30 13:02:08 -0400

    Error handling --- commentss

--- main.js
+++ main.js
@@ -9,16 +9,18 @@ class TwitchPubsub extends EventEmitter {
    * Constructor
    * TODO define what options are needed/wanted for input by user
    * @param {Object} options - JSON object of required options
-   * @param {Boolean} options.reconnect - True to try to reconnect, false to not
+   * @param {boolean} options.reconnect - True to try to reconnect, false to not
    * @param {Object} options.init_topics - JSON Object array of initial topic(s0
    * @param {string} options.init_topics.topic - Topic to listen too
    * @param {string} options.init_topics.token - Authentication token
-   * @param {string} options.url - URL of WS to connect too. DEFAULT: Twitch
+   * @param {boolean} options.debug - Turns debug logging on and off
+   * @param {string} options.url - URL of WS to connect too. DEFAULT: Twitch {"wss://pubsub-edge.twitch.tv"}
    */
-  constructor(options = {reconnect: true, init_topics: {}, url: 'wss://pubsub-edge.twitch.tv'}) {
+  constructor(options = {reconnect: true, init_topics: {}, debug: false, url: 'wss://pubsub-edge.twitch.tv'}) {
     super();
     if(_.isEmpty(options.init_topics)) throw new Error('Missing initial topics');
     this._recon = options.reconnect;
+    this._debug = options.debug;
     this._url = options.url;
 
     this._init_topics = options.init_topics;
@@ -40,7 +42,7 @@ class TwitchPubsub extends EventEmitter {
 
     this._ws.on('open', function open() {
       this._init_nonce = shortid.generate();
-      this.addTopic(this._init_topics, true); // TODO IF INIT IS TRUE THEN SET NONCE TO SEND AS _INIT_NONCE
+      this.addTopic(this._init_topics, true); // TODO @addTopic IF INIT IS TRUE THEN SET NONCE TO SEND AS _INIT_NONCE
     });
 
     this._ws.on('message', function inc(message) {
@@ -49,11 +51,12 @@ class TwitchPubsub extends EventEmitter {
       if(message.type === 'RESPONSE') {
         if(message.nonce !== this._init_nonce) {
           this._init_nonce = null;
+          this._handleError('MESSAGE RESPONSE', message.error);
           this.emit('error', 'Error while listening to initial topics', message.error);
         } else {
           if(this._pending[message.nonce]) {
             if (message.error !== ""){
-               this._pending[message.nonce].reject(message.error);
+               this._pending[message.nonce].reject(message.error); // TODO IN ADDTOPICS MAKE RESOLVE CALLBACK add to _topics
             } else {
               this._pending[message.nonce].resolve(); // TODO IN ADDTOPICS MAKE RESOLVE CALLBACK add to _topics
               delete this._pending[message.nonce];
@@ -122,6 +125,16 @@ class TwitchPubsub extends EventEmitter {
    *
    */
   _onVideoPlayback(message){
+
+  }
+
+  /**
+   * Handles error
+   * @param {string} origin - Name of what callback function error originates frmo
+   * @param {string} error - Error message to emit
+   */
+  _handleError(origin, error){
+    
 
   }
 
